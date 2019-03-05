350137d76f7948de762359256762159e6b5b5182
Roman Bulinski <aboandrus@gmail.com>
2019-03-05 07:33:22 +0100

    Implemented code and tests.

--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -1,2 +1,18 @@
 public class SelectionSort {
+
+
+    public int[] selectionSortArray(int[] arr){
+
+        for( int i=0; i<arr.length; i++ ){
+            for( int j=arr.length-1; j>0; j-- ){
+                if( arr[j-1] > arr[j] ){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
 }
--- src/test/java/TestSelectionSort.java
+++ src/test/java/TestSelectionSort.java
@@ -1,10 +1,33 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestSelectionSort {
 
     @Test
-    public void testFun(){
-
+    public void testFunSelectionSortArray(){
+        SelectionSort selectionSort = new SelectionSort();
+        int[] arr = {3,2,4,1};
+        int[] sortArr = {1,2,3,4};
+        assertArrayEquals(  sortArr, selectionSort.selectionSortArray(selectionSort.selectionSortArray(arr)) );
     }
 
+
+    @Test
+    public void testFunSelectionSortArray2(){
+        SelectionSort selectionSort = new SelectionSort();
+        int[] arr = {52,3,2};
+        int[] sortArr = {2,3,52};
+        assertArrayEquals(  sortArr, selectionSort.selectionSortArray(selectionSort.selectionSortArray(arr)) );
+    }
+
+
+    @Test
+    public void testFunSelectionSortArray3(){
+        SelectionSort selectionSort = new SelectionSort();
+        int[] arr =  {4,12,4,78,7,-1};
+        int[] sortArr = {-1,4,4,7,12,78};
+        assertArrayEquals(  sortArr, selectionSort.selectionSortArray(selectionSort.selectionSortArray(arr)) );
+    }
 }
